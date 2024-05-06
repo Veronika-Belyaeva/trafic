98abe82cdc7100127a64689cc273342e148e03fe
Veronika-Belyaeva <b.v.2011@mail.ru>
2024-05-06 15:16:51 +0700

    add: signup and signin

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const modal = document.querySelector(".modal");
+const modalDialog = document.querySelector(".modal-dialog");
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" || 
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) && modal.classList.contains("is-open")) 
+  ) {
+    event.preventDefault();
+    modal.classList.toggle("is-open");
+  }
+})
+document.addEventListener("keyup", (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
+  }
+})
+
+const wrapper = document.querySelector(".wrapper"),
+signupHeader = document.querySelector(".signup header"),
+loginHeader = document.querySelector(".login header");
+
+loginHeader.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+signupHeader.addEventListener("click", () => {
+  wrapper.classList.remove("active"); 
+});
+
